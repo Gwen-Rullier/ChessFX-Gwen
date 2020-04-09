a2c06bbc842b549013b5a386446a99ea99feb844
Gwen-Rullier <laurent.gwenaelle@gmail.com>
2020-04-09 15:26:33 +0200

    2ème commit : autre essai

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/View/echiquier.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Mon échiquier");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
